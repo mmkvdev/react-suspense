7c1aa8f386a56d4e6ff7d455665ad4c9f20008c1
mmkvdev <madhuv299@gmail.com>
2021-04-11 14:37:26 +0530

    changed background based on randomColorGenerator property and added a image container for batman svg

--- src/components/ToggleWrapper.js
+++ src/components/ToggleWrapper.js
@@ -1,19 +1,24 @@
 import * as React from 'react';
+import { batmanSvg, randomColorGenerator } from '../utils/randomColorGenerator';
 import Loader from '../common/Loader';
 
 const AsyncToggler = React.lazy(() => import ('../common/ToggleSwitch'));
 
-function ToggleWrapper () {
+function ToggleWrapper (props) {
     const [onToggle, setOnToggle] = React.useState(false);
 
     const handleToggler= (onCheck) => {
+        document.body.style.background = randomColorGenerator();
         return setOnToggle(onCheck);
     }
 
     return (
         <React.Suspense fallback={<Loader />}>
             <div className="toggle-container">
-                <AsyncToggler handleToggler={handleToggler} onToggle={onToggle} />
+                <AsyncToggler handleToggler={handleToggler} onToggle={onToggle} onColor={randomColorGenerator()}/>
+            </div>
+            <div className="content-container">
+                <img className="image-container" src={batmanSvg} alt="batman"/>
             </div>
         </React.Suspense>
         
