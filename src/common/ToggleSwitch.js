46dbfb956d7d29155d27824cf0e6c821ccb0c3de
mmkvdev <madhuv299@gmail.com>
2021-04-11 14:35:41 +0530

    updated configuration for handling icons and box shadow with inset properties

--- src/common/ToggleSwitch.js
+++ src/common/ToggleSwitch.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import Switch from 'react-switch';
+import { nightMan } from '../utils/randomColorGenerator';
 
 const ToggleSwitch = (props) => {
     return (
-        <Switch onChange={props.handleToggler} checked={props.onToggle} checkedIcon={false} uncheckedIcon={false} height={35} width={72}  boxShadow="10px 5px 5px black" />
+        <Switch 
+            onChange={props.handleToggler} 
+            checked={props.onToggle} 
+            checkedIcon={false} 
+            uncheckedIcon={false} 
+            checkedHandleIcon={nightMan} 
+            uncheckedHandleIcon={nightMan}
+            height={35} 
+            width={72}  
+            borderRadius={100} 
+            onColor={props.onColor} 
+            offColor={props.onColor} 
+            boxShadow={`0 0 0.5rem ${props.onColor}, inset 0 0 0.5rem ${props.onColor}, 0 0 2rem ${props.onColor}`}
+        />
     );
 }
 
